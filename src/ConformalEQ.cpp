635ce413c56f29955c40f809d09b7c4d0679beb1
Champ Darabundit <champdarabundit@Champs-MacBook-Pro-2.local>
2021-09-27 15:48:53 -0700

    Fixing cmake stuff

--- src/ConformalEQ.cpp
+++ src/ConformalEQ.cpp
@@ -1,1 +1,15 @@
 // Conformal EQ - Champ Darabundit 09/27/21
+
+#include "ConformalEQ.h"
+
+void ConformalEQ::prepareToPlay(double sampleRate, int samplesPerBlock)
+{}
+
+void ConformalEQ::releaseResources()
+{}
+
+void ConformalEQ::processAudioBlock(AudioBuffer<float>& buffer)
+{}
+
+
+
--- src/ConformalEQ.h
+++ src/ConformalEQ.h
@@ -1,1 +1,20 @@
 // Conformal EQ - Champ Darabundit 09/27/21
+
+#pragma once
+
+#include <JuceHeader.h>
+
+class ConformalEQ : public AudioProcessor
+{
+public: 
+	ConformalEQ() {}
+
+    static void addParameters (juce::AudioProcessorValueTreeState::Parameter& params) {}
+	void prepareToPlay (double sampleRate, int samplesPerBlock) override;
+	void releaseResources() override;
+    void processAudioBlock (AudioBuffer<float>& buffer);
+private: 
+	AudioBuffer<float> monoBuffer;
+
+	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ConformalEQ)
+};
